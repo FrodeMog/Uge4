62875f39aaa484ceb80c9ca29a23733ae8d861a4
Frederik Mogensen <frode.moge@gmail.com>
2024-02-26 10:54:33 +0100

    Catch file not found

--- generatorReadMidi.py
+++ generatorReadMidi.py
@@ -9,14 +9,20 @@ class GeneratorReadMidi:
         try:
             mid = mido.MidiFile(file_path)
             return list(mid)
+        except FileNotFoundError as e:
+            print(f"Error reading MIDI file {file_path}: {e}")
+            return None
         except Exception as e:
             return e
         
-    # Read using generator
+    # Read using generator yield
     def readMidiGenerator(self, file_path):
         try:
             mid = mido.MidiFile(file_path)
             for item in mid:
                 yield item
+        except FileNotFoundError as e:
+            print(f"Error reading MIDI file {file_path}: {e}")
+            return None
         except Exception as e:
             return e
