7ede6f3c6ac887bfffdd05c746535e5097edae94
Frederik Mogensen <frode.moge@gmail.com>
2024-02-26 10:25:59 +0100

    get ready to read and plot midi

--- generatorReadMidi.py
+++ generatorReadMidi.py
@@ -2,6 +2,21 @@ import mido
 
 class GeneratorReadMidi:
     def __init__(self):
+        pass
 
+    # Read using regular return
     def readMidi(self, file_path):
+        try:
+            mid = mido.MidiFile(file_path)
+            return list(mid)
+        except Exception as e:
+            return e
         
+    # Read using generator
+    def readMidiGenerator(self, file_path):
+        try:
+            mid = mido.MidiFile(file_path)
+            for item in mid:
+                yield item
+        except Exception as e:
+            return e
