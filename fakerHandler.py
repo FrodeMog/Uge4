720546a68d2d95dba636d02980d83160fbf84f22
FrodeMog <96060147+FrodeMog@users.noreply.github.com>
2024-02-27 18:17:24 +0100

    fake data example

--- fakerHandler.py
+++ fakerHandler.py
@@ -1,4 +1,24 @@
 class FakerHandler:
     def __init__(self):
         pass
+
+    def readCSV(self, file_path):
+        try:
+            with open(file_path, 'r') as f:
+                return f.readlines()
+        except FileNotFoundError as e:
+            print(f"Error reading CSV file {file_path}: {e}")
+            return None
+        except Exception as e:
+            return e
     
+    def readCSVGenerator(self, file_path):
+        try:
+            with open(file_path, 'r') as f:
+                for line in f:
+                    yield line
+        except FileNotFoundError as e:
+            print(f"Error reading CSV file {file_path}: {e}")
+            return None
+        except Exception as e:
+            return e
