d96a3332490c5a37879d8d2010f7bf1ab792ee5c
FrodeMog <96060147+FrodeMog@users.noreply.github.com>
2024-03-01 13:42:07 +0100

    fix time measure error

--- Utils.py
+++ Utils.py
@@ -7,13 +7,13 @@ class Utils:
 
     # Measure the time and memory usage of a function
     def measure_function(func, *args, **kwargs):
-        t1 = time.process_time()
         mem_before = mem_profile.memory_usage()
 
         # Function to measure
+        start_time = time.time()
         func(*args, **kwargs)
+        end_time = time.time()
 
         mem_after = mem_profile.memory_usage()
-        t2 = time.process_time()
         print("Memory used: " + str(mem_after[0] - mem_before[0]) + " MB")
-        print("Time taken: " + str(t2-t1) + " seconds"+"\n")
+        print("Time taken: " + str(end_time - start_time) + " seconds" + "\n")
